98dd82c15b2e92e39a919ded5328843f6af01d4a
root <cebril@gmail.com>
2019-03-10 14:34:27 +0000

    add class features

---
+++ class_features.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from pythainlp.tokenize import word_tokenize
+
+def top_feats(row, features, top_n=25):
+    ''' Get top n tfidf values in row and return them with their corresponding feature names.'''
+    topn_ids = np.argsort(row)[::-1][:top_n]
+    top_feats = [(features[i], row[i]) for i in topn_ids]
+    df = pd.DataFrame(top_feats)
+    df.columns = ['feature', 'score']
+    return df
+
+def top_mean_feats(Xtr, features, grp_ids=None, min_tfidf=0.1, top_n=25):
+    ''' Return the top n features that on average are most important amongst documents in rows
+        indentified by indices in grp_ids. '''
+    if grp_ids:
+        D = Xtr[grp_ids].toarray()
+    else:
+        D = Xtr.toarray()
+
+    D[D < min_tfidf] = 0
+    tfidf_means = np.mean(D, axis=0)
+    return top_feats(tfidf_means, features, top_n)
+
+def top_feats_by_class(Xtr, y, features, min_tfidf=0.1, top_n=25):
+    ''' Return a list of dfs, where each df holds top_n features and their mean tfidf value
+        calculated across documents with the same class label. '''
+    dfs = []
+    labels = np.unique(y)
+    for label in labels:
+        ids = np.where(y==label)
+        feats_df = top_mean_feats(Xtr, features, ids, min_tfidf=min_tfidf, top_n=top_n)
+        feats_df.label = label
+        dfs.append(feats_df)
+    return dfs
+
+def plot_classfeats_h(dfs, nrow = 2, ncol=4):
+    ''' Plot the data frames returned by the function plot_tfidf_classfeats(). '''
+    fig = plt.figure(figsize=(24, 18), facecolor="w")
+    x = np.arange(len(dfs[0]))
+    for i, df in enumerate(dfs):
+        ax = fig.add_subplot(nrow, ncol, i+1)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.set_frame_on(False)
+        ax.get_xaxis().tick_bottom()
+        ax.get_yaxis().tick_left()
+        ax.set_title(str(df.label), fontsize=12)
+        ax.ticklabel_format(axis='x', style='sci', scilimits=(-2,2))
+        ax.barh(x, df.score, align='center', color='#3F5D7D')
+        ax.set_yticks(x)
+        ax.set_ylim([-1, x[-1]+1])
+        ax.invert_yaxis()
+        yticks = ax.set_yticklabels(df.feature)
+        plt.subplots_adjust(bottom=0.09, right=0.97, left=0.15, top=0.95, wspace=0.52)
+    plt.show()
+    
+def show_classfeats(df, vectorizer, analyzer, text_col='texts', class_col='category',
+                    nrow=2, ncol=4):
+    vec = vectorizer(analyzer=analyzer)
+    mat = vec.fit_transform(df[text_col])
+    dfs = top_feats_by_class(mat, df[class_col], vec.get_feature_names())
+    plot_classfeats_h(dfs,nrow,ncol)
